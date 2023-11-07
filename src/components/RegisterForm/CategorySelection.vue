69cf6b6be8cb941275e49c28c60b81c5ddb995b2
Bakhodur Nazriev <bakhodur_nazriev@ummalife.com>
2023-11-08 01:13:06 +0500

    Improve CategorySelection.vue and make minor refactoring

--- src/components/RegisterForm/CategorySelection.vue
+++ src/components/RegisterForm/CategorySelection.vue
@@ -4,7 +4,6 @@ import FormAuth from '@/components/ui/FormAuth.vue'
 import {ref} from 'vue'
 import SampleButton from '@/components/ui/SampleButton.vue'
 
-const categoryCount = ref(0)
 const categoryItems = ref([
   'Религия',
   'Семья',
@@ -28,17 +27,38 @@ const categoryItems = ref([
   'Психология',
   'Воспитание'
 ])
+const selectedCategories = ref([])
+
+const toggleCategory = (category) => {
+  if (isSelected(category)) {
+    selectedCategories.value = selectedCategories.value.filter((c) => c !== category)
+  } else if (selectedCategories.value.length < 5) {
+    selectedCategories.value.push(category)
+  }
+}
+
+const isSelected = (category) => {
+  return selectedCategories.value.includes(category)
+}
 </script>
 
 <template>
   <FormAuth>
-    <h3 class="form-login__interests-title">{{ $t('login.interests_category.title') }}</h3>
+    <h3 class="form-title">{{ $t('login.interests_category.title') }}</h3>
     <p class="form-subtitle">{{ $t('login.interests_category.subtitle') }}</p>
-    <span class="category-count">{{ categoryCount }}/5</span>
+    <span class="category-count">{{ selectedCategories.length }}/5</span>
 
     <div class="category-items__block">
       <ul class="category-list">
-        <li class="category-list__item" v-for="(item, i) in categoryItems" :key="i">{{ item }}</li>
+        <li
+            class="category-list__item"
+            v-for="(item, i) in categoryItems"
+            :key="i"
+            @click="toggleCategory(item)"
+            :class="{ 'interest-selected': isSelected(item) }"
+        >
+          {{ item }}
+        </li>
       </ul>
     </div>
 
@@ -49,11 +69,17 @@ const categoryItems = ref([
 </template>
 
 <style lang="scss" scoped>
-.form-login__interests-title {
+.form-title {
   font-size: 28px;
   margin: 0 0 12px;
   font-weight: 500;
   line-height: normal;
+}
+
+.form-subtitle {
+  font-size: 20px;
+  line-height: 1.2;
+  margin: 0 0 8px;
 }
 
 .ready-button__block {
@@ -67,12 +93,6 @@ const categoryItems = ref([
   display: flex;
   justify-content: center;
   width: 100%;
-}
-
-.form-subtitle {
-  font-size: 20px;
-  line-height: 1.2;
-  margin: 0 0 8px;
 }
 
 .category-count {
@@ -102,7 +122,7 @@ const categoryItems = ref([
     line-height: normal;
 
     &:hover {
-      transition: all 0.15s ease-in-out;
+      transition: all 0.2s ease-in-out;
       background-color: var(--color-alto-first);
     }
   }
@@ -111,5 +131,30 @@ const categoryItems = ref([
 .interest-selected {
   background-color: var(--color-hippie-blue);
   color: var(--color-white);
+
+  &:hover {
+    background-color: var(--color-deep-cerulean);
+    color: var(--color-white);
+    transition: all 0.2s ease-in-out;
+  }
+}
+
+@media (max-width: 576px) {
+  .category-items__block {
+    margin-top: 24px;
+  }
+
+  .form-title {
+    font-size: 22px;
+  }
+
+  .form-subtitle,
+  .category-count {
+    font-size: 16px;
+  }
+
+  .ready-button__block {
+    margin-top: 40px;
+  }
 }
 </style>
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,51 +1,46 @@
-<template>
-  <LayoutAuth>
-    <register-form v-if="currentStep === 'registerForm'" @nextStep="handleNextStep"></register-form>
-    <verify-with-email v-if="currentStep === 'verifyWithEmail'" @nextStep="handleNextStep"></verify-with-email>
-    <create-new-password v-if="currentStep === 'createNewPassword'" @nextStep="handleNextStep"></create-new-password>
-    <base-user-info v-if="currentStep === 'baseUserInfo'" @nextStep="handleNextStep"></base-user-info>
-    <add-phone-and-login v-if="currentStep === 'addPhoneAndLogin'" @nextStep="handleNextStep"></add-phone-and-login>
-  </LayoutAuth>
-</template>
-
-<script>
+<script setup>
+/* eslint-disable */
+import {ref} from 'vue'
 import LayoutAuth from '@/components/layouts/LayoutAuth.vue'
 import VerifyWithEmail from '@/components/RegisterForm/VerifyWithEmail.vue'
 import CreateNewPassword from '@/components/RegisterForm/CreateNewPassword.vue'
 import BaseUserInfo from '@/components/RegisterForm/BaseUserInfo.vue'
 import AddPhoneAndLogin from '@/components/RegisterForm/AddPhoneAndLogin.vue'
 import RegisterForm from '@/components/RegisterForm/RegisterForm.vue'
+import CategorySelection from '@/components/RegisterForm/CategorySelection.vue'
 
-export default {
-  components: {
-    RegisterForm,
-    AddPhoneAndLogin,
-    BaseUserInfo,
-    CreateNewPassword,
-    VerifyWithEmail,
-    LayoutAuth
-  },
-  data () {
-    return {
-      currentStep: 'registerForm'
-    }
-  },
-  methods: {
-    handleNextStep () {
-      if (this.currentStep === 'registerForm') {
-        this.currentStep = 'verifyWithEmail'
-      } else if (this.currentStep === 'verifyWithEmail') {
-        this.currentStep = 'createNewPassword'
-      } else if (this.currentStep === 'createNewPassword') {
-        this.currentStep = 'baseUserInfo'
-      } else if (this.currentStep === 'baseUserInfo') {
-        this.currentStep = 'addPhoneAndLogin'
-      }
-    }
+const currentStep = ref('registerForm')
+
+const handleNextStep = () => {
+  switch (currentStep.value) {
+    case 'registerForm':
+      currentStep.value = 'verifyWithEmail'
+      break
+    case 'verifyWithEmail':
+      currentStep.value = 'createNewPassword'
+      break
+    case 'createNewPassword':
+      currentStep.value = 'baseUserInfo'
+      break
+    case 'baseUserInfo':
+      currentStep.value = 'addPhoneAndLogin'
+      break
+    case 'addPhoneAndLogin':
+      currentStep.value = 'categoryInterests'
+      break
   }
 }
 </script>
 
-<style scoped>
+<template>
+  <LayoutAuth>
+    <RegisterForm v-if="currentStep === 'registerForm'" @nextStep="handleNextStep"/>
+    <VerifyWithEmail v-if="currentStep === 'verifyWithEmail'" @nextStep="handleNextStep"/>
+    <CreateNewPassword v-if="currentStep === 'createNewPassword'" @nextStep="handleNextStep"/>
+    <BaseUserInfo v-if="currentStep === 'baseUserInfo'" @nextStep="handleNextStep"/>
+    <AddPhoneAndLogin v-if="currentStep === 'addPhoneAndLogin'" @nextStep="handleNextStep"/>
+    <CategorySelection v-if="currentStep === 'categoryInterests'" @nextStep="handleNextStep"/>
+  </LayoutAuth>
+</template>
 
-</style>
+<style scoped></style>
